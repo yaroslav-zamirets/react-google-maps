75e42d82b7381b2b5ff5112b530affba83ff9813
Tom Chen <developer@tomchentw.com>
2016-07-15 18:09:20 +0800

    refactor(GoogleMap): rewrite with enhanceElement and cleaner interfaces

--- src/__tests__/index.spec.js
+++ src/__tests__/index.spec.js
@@ -3,6 +3,17 @@ import {
 } from "expect";
 
 import {
+  default as React,
+} from "react";
+
+import {
+  unmountComponentAtNode,
+  render,
+} from "react-dom";
+
+import * as maps from "../__mocks__/google.maps.mock";
+
+import {
   withGoogleMap,
   GoogleMap,
 
@@ -33,5 +44,107 @@ describe(`index`, () => {
     expect(Polyline).toExist();
     expect(Rectangle).toExist();
     expect(SearchBox).toExist();
+  });
+
+  context(`combine withGoogleMap with GoogleMap`, () => {
+    let Component;
+
+    before(() => {
+      global.google = { maps };
+      Component = withGoogleMap(GoogleMap);
+    });
+
+    after(() => {
+      delete global.google;
+    });
+
+    describe(`initialization`, () => {
+      context(`global.google is undefined`, () => {
+        let prevGoogle;
+
+        before(() => {
+          prevGoogle = global.google;
+          delete global.google;
+        });
+
+        after(() => {
+          global.google = prevGoogle;
+        });
+
+        it(`should warn and throw error`, () => {
+          const warningSpy = expect.spyOn(console, `error`);
+          expect(warningSpy).toNotHaveBeenCalled();
+
+          let error;
+          try {
+            render((
+              <Component
+                containerElement={<div />}
+                mapElement={<div />}
+              />
+            ), document.createElement(`div`));
+          } catch (__e__) {
+            error = __e__;
+          }
+          expect(error).toExist();
+          expect(warningSpy).toHaveBeenCalled();
+
+          warningSpy.restore();
+        });
+      });
+    });
+
+    describe(`rendering`, () => {
+      let domEl;
+
+      beforeEach(() => {
+        domEl = document.createElement(`div`);
+      });
+
+      afterEach(() => {
+        unmountComponentAtNode(domEl);
+        domEl = null;
+      });
+
+      it(`should call setOptions during initial render`, () => {
+        const setOptionsSpy = expect.spyOn(maps.Map.prototype, `setOptions`);
+        expect(setOptionsSpy).toNotHaveBeenCalled();
+
+        render((
+          <Component
+            containerElement={<div />}
+            mapElement={<div />}
+          />
+        ), domEl);
+
+        expect(setOptionsSpy).toHaveBeenCalled();
+
+        setOptionsSpy.restore();
+      });
+
+      it(`should call setZoom when props.zoom changes`, () => {
+        const setZoomSpy = expect.spyOn(maps.Map.prototype, `setZoom`);
+        expect(setZoomSpy).toNotHaveBeenCalled();
+
+        render((
+          <Component
+            containerElement={<div />}
+            mapElement={<div />}
+          />
+        ), domEl);
+        expect(setZoomSpy).toNotHaveBeenCalled();
+
+        render((
+          <Component
+            containerElement={<div />}
+            mapElement={<div />}
+            zoom={10}
+          />
+        ), domEl);
+        expect(setZoomSpy).toHaveBeenCalled();
+
+        setZoomSpy.restore();
+      });
+    });
   });
 });
